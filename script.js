cc2ac42c36871b1ec79dd17944c0a4750bf56799
vgd15 <deise171315@gmail.com>
2023-04-22 14:54:33 -0300

    com js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let links = document.getElementById("links");
+links.addEventListener("mouseover", Mouse)
+links.addEventListener("mouseout", Mouse1)
+
+function Mouse(){
+    links.style.color="red";
+}
+function Mouse1(){
+    links.style.color="";
+}
 
 
 
